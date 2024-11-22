4eeae028cb5ecb3b2b88059c3caa487ac8d2ab11
Mathilde Mousset <mathilde.mousset@epicentre.msf.org>
2024-11-22 12:59:07 +0100

    Fix paths
    I am not sure what happened here.

--- R/copy_data_to_repicentre.R
+++ R/copy_data_to_repicentre.R
@@ -15,32 +15,35 @@
 
 # user specific paths
 user <- Sys.info()[["user"]]
+paths <- list()
 paths$user <- user
 if (user == "hugzsoubrier") {
   paths$simulated_data_repo_path <- "/Users/hugzsoubrier/GitHub/simulated-data"
   paths$repicentre_repo_path <- "/Users/hugzsoubrier/GitHub/repicentre"
+} else if (user == "M-MOUSSET") {
+    paths$simulated_data_repo_path <- "D:/MATHILDE_UNPLUGGED/5_OTHER/simulated-data"
+    paths$repicentre_repo_path <- "D:/MATHILDE_UNPLUGGED/5_OTHER/repicentre"
 } else {
-  
   stop("Enter your simulated_data and repicentre repository path here")
-
 }
 
 # check that the data folder exists in repicentre otherwise creates it
-fs::dir_exists(fs::path(repicentre_repo_path, "data", "clean") ) 
-fs::dir_exists(fs::path(repicentre_repo_path, "data", "raw"))
+fs::dir_exists(fs::path(paths$simulated_data_repo_path, "data", "clean") ) 
+fs::dir_exists(fs::path(paths$simulated_data_repo_path, "data", "raw"))
 
 # fetch only the simulated LL from the clean data folder in simulated_data repository 
-clean_ll <- fs::dir_ls(fs::path(data_repo_path, "data", "clean"), regexp = ".*/simulated_measles[^/]*\\.rds")
+clean_ll <- fs::dir_ls(fs::path(paths$simulated_data_repo_path, "data", "clean"), 
+                       regexp = ".*/simulated_measles[^/]*\\.rds")
 
 # fetch only the simulated LL from the clean data folder in simulated_data repository 
-raw_ll <- fs::dir_ls(fs::path(data_repo_path, "data", "final"))
+raw_ll <- fs::dir_ls(fs::path(paths$simulated_data_repo_path, "data", "final"))
 
 # copy clean files
 for(i in clean_ll){
   
   fs::file_copy(
     path = i, 
-    new_path = fs::path(repicentre_repo_path, "data", "clean"), 
+    new_path = fs::path(paths$repicentre_repo_path, "data", "clean"), 
     overwrite = TRUE
                 )
 }
@@ -50,7 +53,7 @@ for(i in raw_ll){
   
   fs::file_copy(
     path = i, 
-    new_path = fs::path(repicentre_repo_path, "data", "raw"), 
+    new_path = fs::path(paths$repicentre_repo_path, "data", "raw"), 
     overwrite = TRUE
   )
   
