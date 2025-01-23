347641624fd7954a0d43dd6941a87ac45002ed71
hugzsoubrier <soubrierhugo@gmail.com>
2025-01-23 09:26:51 +0100

    fixes in copy script

--- R/copy_data_to_repicentre.R
+++ R/copy_data_to_repicentre.R
@@ -36,10 +36,10 @@ fs::dir_create(fs::path(paths$repicentre_repo_path, "data", "dictionary"))
 clean_ll <- fs::dir_ls(fs::path(paths$simulated_data_repo_path, "data", "clean"), regexp = ".*/simulated_measles[^/]*\\.rds")
 
 # fetch only the simulated LL from the clean data folder in simulated_data repository 
-raw_ll <- fs::dir_ls(fs::path(paths$simulated_data_repo_path, "data", "final"))
+raw_dir <- fs::path(paths$simulated_data_repo_path, "data", "final")
 
 # fetch only the simulated LL from the clean data folder in simulated_data repository 
-dict_ll <- fs::dir_ls(fs::path(paths$simulated_data_repo_path, "data", "dictionary"))
+dict_dir <- fs::path(paths$simulated_data_repo_path, "data", "dictionary")
 
 # copy clean files
 for(i in clean_ll){
@@ -52,23 +52,15 @@ for(i in clean_ll){
 }
 
 #copy raw files 
-for(i in raw_ll){
-  
   fs::dir_copy(
-    path = i, 
+    path = raw_dir, 
     new_path = fs::path(paths$repicentre_repo_path, "data", "raw"), 
     overwrite = TRUE
   )
-  
-}
 
-#copy dict files 
-for(i in dict_ll){
-  
-  fs::file_copy(
-    path = i, 
+#copy dict dir 
+fs::file_copy(
+    path = dict_dir, 
     new_path = fs::path(paths$repicentre_repo_path, "data", "dictionary"), 
     overwrite = TRUE
   )
-  
-}
